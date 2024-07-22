0803255d1de9c17111bd83aa7d0c13687db17850
kaixi287 <bao.kaixi@gmail.com>
2024-07-22 15:35:21 +0200

    Reduce time efficiency rewards weight

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/pose_tracking/config/anymal_d/pos_tracking_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/pose_tracking/config/anymal_d/pos_tracking_env_cfg.py
@@ -28,8 +28,8 @@ import omni.isaac.lab_tasks.manager_based.navigation.mdp as mdp_nav
 from omni.isaac.lab_tasks.manager_based.locomotion.velocity.config.anymal_d.flat_env_cfg import AnymalDFlatEnvCfg
 
 LOW_LEVEL_ENV_CFG = AnymalDFlatEnvCfg()
-EPISODE_LENGTH_S = 8.0
-T_R = 1.5
+EPISODE_LENGTH_S = 6.0
+T_R = 1.0
 
 ##
 # Pre-defined configs
@@ -152,15 +152,15 @@ class EventCfg:
         },
     )
 
-    block_joint = EventTerm(
-        func=mdp.block_joint,
-        mode="reset",
-        params={
-            "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
-            "joint_to_block": -1, # Index of joint to disable
-            "prob_no_block": 0.2,
-        },
-    )
+    # block_joint = EventTerm(
+    #     func=mdp.block_joint,
+    #     mode="reset",
+    #     params={
+    #         "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
+    #         "joint_to_block": -1, # Index of joint to disable
+    #         "prob_no_block": 0.2,
+    #     },
+    # )
 
 @configclass
 class RewardsCfg:
@@ -203,7 +203,7 @@ class RewardsCfg:
     # -- reward for time efficiency
     time_efficiency_reward = RewTerm(
         func=mdp.time_efficiency_reward,
-        weight=5.0,
+        weight=1.0,
         params={"T": EPISODE_LENGTH_S, "command_name": "pose_command"}
     )
 
