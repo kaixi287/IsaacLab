f8f6c5532b95d673b08e9976257635ab415f8ca1
kaixi827 <bao.kaixi@gmail.com>
2024-09-15 11:42:09 +0200

    Add learning rate scheduler arguments

--- run_training.sh
+++ run_training.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-# Command template
-command="./docker/cluster/cluster_interface.sh job base --task Isaac-PosTracking-Flat-Anymal-D-v0 --headless --logger wandb --run_name mlp_aug_time --log_project_name position_tracking5 --log_run_name mlp_aug"
+# # Command template
+# command="./docker/cluster/cluster_interface.sh job base --task Isaac-PosTracking-Flat-Anymal-D-v0 --headless --logger wandb --run_name mlp_aug_time --log_project_name position_tracking5 --log_run_name mlp_aug"
 
-sleep 11000
+# sleep 11000
+# # Loop through seeds 1 to 5
+# for seed in {2..5}; do
+#     # sleep 3600
+#     # Execute the command with the current seed
+#     $command --seed $seed
+    
+#     # Wait for 10 minutes (600 seconds) before running the next command
+#     sleep 400
+# done
+
+# Command template
+rnn_command="./docker/cluster/cluster_interface.sh job base --task Isaac-PosTracking-Flat-Anymal-D-v0 --headless --logger wandb --run_name rnn_aug_mid --log_project_name position_tracking3 --log_run_name rnn_aug --actor_critic_class ActorCriticRecurrent"
+
 # Loop through seeds 1 to 5
-for seed in {2..5}; do
-    # sleep 3600
+for seed in {1..2}; do
+    # sleep 600
     # Execute the command with the current seed
-    $command --seed $seed
+    $rnn_command --seed $seed
     
     # Wait for 10 minutes (600 seconds) before running the next command
-    sleep 400
+    sleep 300
 done
 
 # Command template
-# rnn_command="./docker/cluster/cluster_interface.sh job base --task Isaac-PosTracking-Flat-Anymal-D-v0 --headless --logger wandb --run_name rnn_aug_time --log_project_name position_tracking5 --log_run_name rnn_aug --actor_critic_class_name ActorCriticRecurrent"
+rnn_command="./docker/cluster/cluster_interface.sh job base --task Isaac-PosTracking-Flat-Anymal-D-v0 --headless --logger wandb --run_name rnn_aug_mid --log_project_name position_tracking3 --log_run_name rnn_aug --actor_critic_class ActorCriticRecurrent --lr_step_size 500 --lr_decay 0.5"
 
-# # Loop through seeds 1 to 5
-# for seed in {1..5}; do
-#     # sleep 600
-#     # Execute the command with the current seed
-#     $rnn_command --seed $seed
+# Loop through seeds 1 to 5
+for seed in {1..2}; do
+    # sleep 600
+    # Execute the command with the current seed
+    $rnn_command --seed $seed
     
-#     # Wait for 10 minutes (600 seconds) before running the next command
-#     sleep 600
-# done
+    # Wait for 10 minutes (600 seconds) before running the next command
+    sleep 300
+done
--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/position/config/anymal_d/agents/rsl_rl_ppo_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/position/config/anymal_d/agents/rsl_rl_ppo_cfg.py
@@ -39,6 +39,8 @@ class AnymalDPosTrackingEnvPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         lam=0.95,
         desired_kl=0.01,
         max_grad_norm=1.0,
+        lr_step_size=600,
+        lr_decay=0.5,
         # -- Symmetry Augmentation
         symmetry_cfg=dict(
             use_data_augmentation=True,  # this adds symmetric trajectories to the batch
