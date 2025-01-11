bf3ce8dfb09fe331a2c6b7bd23f59ac88e0c59c8
kaixi827 <bao.kaixi@gmail.com>
2025-01-11 14:49:54 +0100

    Add rough terrain positio tracking registration

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/position/config/anymal_d/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/position/config/anymal_d/__init__.py
@@ -1,11 +1,11 @@
-# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
 # All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
 import gymnasium as gym
 
-from . import agents, flat_env_cfg
+from . import agents, flat_env_cfg, rough_env_cfg
 
 ##
 # Register Gym environments.
@@ -27,6 +27,26 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": flat_env_cfg.AnymalDPosTrackingFlatEnvCfg_PLAY,
+        "rsl_rl_cfg_entry_point": agents.rsl_rl_ppo_cfg.AnymalDPosTrackingEnvPPORunnerCfg,
+    },
+)
+
+gym.register(
+    id="Isaac-PosTracking-Rough-Anymal-D-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": rough_env_cfg.AnymalDPosTrackingRoughEnvCfg,
+        "rsl_rl_cfg_entry_point": agents.rsl_rl_ppo_cfg.AnymalDPosTrackingEnvPPORunnerCfg,
+    },
+)
+
+gym.register(
+    id="Isaac-PosTracking-Rough-Anymal-D-Play-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": rough_env_cfg.AnymalDPosTrackingRoughEnvCfg_PLAY,
         "rsl_rl_cfg_entry_point": agents.rsl_rl_ppo_cfg.AnymalDPosTrackingEnvPPORunnerCfg,
     },
 )
